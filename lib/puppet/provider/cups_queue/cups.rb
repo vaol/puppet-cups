7ee0b8ba30569b327f68c3adf47adc0e44aac49d
VAOL <olivier.vallejo@swiss-as.com>
2026-07-08 11:29:21 +0200

    add job-cancel-after

--- lib/puppet/provider/cups_queue/cups.rb
+++ lib/puppet/provider/cups_queue/cups.rb
@@ -277,7 +277,7 @@ Puppet::Type.type(:cups_queue).provide(:cups) do
     answer = {}
 
     options = [
-      'auth-info-required', 'job-k-limit', 'job-page-limit', 'job-quota-period', 'job-sheets-default', 'port-monitor', 'printer-error-policy', 'printer-op-policy', 'job-cancel-after-default'
+      'auth-info-required', 'job-k-limit', 'job-page-limit', 'job-quota-period', 'job-sheets-default', 'port-monitor', 'printer-error-policy', 'printer-op-policy', 'job-cancel-after-default', 'job-cancel-after'
     ]
 
     options.each { |option| answer[option] = query_native_option(option) }
