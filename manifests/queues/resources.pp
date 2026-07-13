3966c105ec88b2ef3629d862a883757d72cdfe7c
VAOL <olivier.vallejo@swiss-as.com>
2026-07-13 23:12:04 +0200

    default URL options

--- manifests/queues/resources.pp
+++ manifests/queues/resources.pp
@@ -9,6 +9,9 @@
 # Merges `default_options` with resource-level options, with resource-level
 # options taking precedence.
 #
+# Merges `default_url_parameters` with resource-level url_parameters, with resource-level
+# url_parameters taking precedence. Appends the merged parameters to the URI.
+#
 # @author Leo Arnold
 # @since 2.0.0
 #
@@ -21,15 +24,36 @@
 #
 class cups::queues::resources {
   if ($cups::resources) {
-    if ($cups::default_options) {
-      $merged_resources = $cups::resources.map |$name, $attrs| {
-        $resource_options = if $attrs['options'] { $attrs['options'] } else { {} }
-        $merged_options = $cups::default_options + $resource_options
-        [$name, $attrs + { 'options' => $merged_options }]
-      }.convert_to(Hash)
-      create_resources('cups_queue', $merged_resources)
-    } else {
-      create_resources('cups_queue', $cups::resources)
+    # Helper function to merge URL parameters into URI
+    $build_resource = lambda |$name, $attrs| {
+      $resource_options = if $attrs['options'] { $attrs['options'] } else { {} }
+      $merged_options = if $cups::default_options {
+        $cups::default_options + $resource_options
+      } else {
+        $resource_options
+      }
+
+      # Handle URL parameter merging
+      $resource_url_params = if $attrs['url_parameters'] { $attrs['url_parameters'] } else { {} }
+      $merged_url_params = if $cups::default_url_parameters {
+        $cups::default_url_parameters + $resource_url_params
+      } else {
+        $resource_url_params
+      }
+
+      # Build final URI with URL parameters appended
+      $final_uri = if !empty($merged_url_params) {
+        # Convert hash to query string: {timeout: 20, foo: bar} -> /timeout=20&foo=bar
+        $query_params = $merged_url_params.map |$key, $value| { "${key}=${value}" }.join('&')
+        "${attrs['uri']}/${query_params}"
+      } else {
+        $attrs['uri']
+      }
+
+      [$name, $attrs + { 'options' => $merged_options, 'uri' => $final_uri }]
     }
+
+    $merged_resources = $cups::resources.map($build_resource).convert_to(Hash)
+    create_resources('cups_queue', $merged_resources)
   }
 }
